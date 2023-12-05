a3a75d51bda0cda2d069e16533a61a3248399db3
Ryan Hampton <hamptonryan88@gmail.com>
2023-12-05 09:18:47 -0600

    adding .gitignore

---
+++ src/main/java/com/example/codeupspringcapstone/CodeupSpringCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeupspringcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codeupspringcapstone/CodeupSpringCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeupspringcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
