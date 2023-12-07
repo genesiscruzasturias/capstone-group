91a23bdddfc33d97bd697e3f7cc38eecd882f55d
Ryan Hampton <hamptonryan88@gmail.com>
2023-12-07 11:34:05 -0600

    can now see individual breweries -rh

--- src/main/resources/static/js/brewery_info.js
+++ src/main/resources/static/js/brewery_info.js
@@ -1,13 +1,10 @@
-window.onload = function() {
+
     const urlParams = new URLSearchParams(window.location.search);
-    const websiteUrl = urlParams.get('url');
+    const breweryID = urlParams.get('brewery');
 
-    if (websiteUrl) {
-        fetch('https://api.openbrewerydb.org/v1/breweries?by_website=' + encodeURIComponent(websiteUrl))
+fetch('https://api.openbrewerydb.org/v1/breweries/' + breweryID)
             .then(response => response.json())
-            .then(data => {
-                const brewery = data[0];
-
+            .then(brewery => {
                 const formattedPhone = brewery.phone ? formatPhoneNumber(brewery.phone) : 'N/A';
 
                 const breweryInfoDiv = document.getElementById('breweryInfo');
@@ -15,17 +12,15 @@ window.onload = function() {
                     <h2>${brewery.name}</h2>
                     <p>
                         Type: ${brewery.brewery_type}<br>
-                        Address: ${brewery.street ? brewery.street : 'N/A'}, 
-                                 ${brewery.city}, 
-                                 ${brewery.state}, 
+                        Address: ${brewery.street ? brewery.street : 'N/A'},
+                                 ${brewery.city},
+                                 ${brewery.state},
                                  ${brewery.postal_code}<br>
                         Website: ${brewery.website_url ? `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>` : 'N/A'}<br>
                         Phone: ${formattedPhone}
                     </p>`;
             })
             .catch(error => console.error('Error:', error));
-    }
-};
 
 function formatPhoneNumber(phoneNumber) {
     const cleaned = ('' + phoneNumber).replace(/\D/g, '');
