c12a9f0d8a58800a22f6c1d3526975dd6857e6d4
Daniel Deane <daniel.michael.deane@gmail.com>
2023-12-07 12:25:04 -0600

    added a create review btn on brewery_info.js, deleted sign-out.html and changed then name of create-post.html to create-review.html. When i click on the create review btn i get redirected to create-review however i get a 403 error. I will work on it after lunch.

--- src/main/resources/static/js/brewery_info.js
+++ src/main/resources/static/js/brewery_info.js
@@ -2,13 +2,13 @@
     const urlParams = new URLSearchParams(window.location.search);
     const breweryID = urlParams.get('brewery');
 
-fetch('https://api.openbrewerydb.org/v1/breweries/' + breweryID)
-            .then(response => response.json())
-            .then(brewery => {
-                const formattedPhone = brewery.phone ? formatPhoneNumber(brewery.phone) : 'N/A';
+    fetch('https://api.openbrewerydb.org/v1/breweries/' + breweryID)
+        .then(response => response.json())
+        .then(brewery => {
+            const formattedPhone = brewery.phone ? formatPhoneNumber(brewery.phone) : 'N/A';
 
-                const breweryInfoDiv = document.getElementById('breweryInfo');
-                breweryInfoDiv.innerHTML = `
+            const breweryInfoDiv = document.getElementById('breweryInfo');
+            breweryInfoDiv.innerHTML = `
                     <h2>${brewery.name}</h2>
                     <p>
                         Type: ${brewery.brewery_type}<br>
@@ -18,11 +18,13 @@ fetch('https://api.openbrewerydb.org/v1/breweries/' + breweryID)
                                  ${brewery.postal_code}<br>
                         Website: ${brewery.website_url ? `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>` : 'N/A'}<br>
                         Phone: ${formattedPhone}
-                    </p>`;
-            })
-            .catch(error => console.error('Error:', error));
+                    </p>
+                    <button onclick="location.href='/create-review.html';">Create Review</button>`;
+        })
+        .catch(error => console.error('Error:', error));
 
-function formatPhoneNumber(phoneNumber) {
+
+    function formatPhoneNumber(phoneNumber) {
     const cleaned = ('' + phoneNumber).replace(/\D/g, '');
     const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
     if (match) {
